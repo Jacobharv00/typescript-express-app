0b665d636e0ed8f188d1893c879b7c6b256a44dc
Jacob Harvey <jharvey@mahalobanking.com>
2024-05-17 09:15:15 -0600

    feat: add first login route

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 import express from 'express'
+import bodyParser from "body-parser";
 
-import type {Request, Response} from 'express'
+import {router} from "./routes/loginRoutes";
 
 const app = express()
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(`
-        <div>
-            <h1>Hi there!</h1>
-        </div>
-    `)
-})
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(router)
 
 app.listen(3000, () => {
     console.log('** Listening on port 3000 **')
