cfd0f9486367cd848436062cabd2f12117b62a46
Jacob Harvey <jharvey@mahalobanking.com>
2024-05-21 08:58:13 -0600

    feat: setup first decorator controller and get

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import express from 'express'
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
 
+import './controllers/LoginControllers'
+import {router as controllerRouter} from './controllers/decorators/controller'
+
 import {router} from "./routes/loginRoutes";
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieSession({keys: ['asdfjkl']}))
 app.use(router)
+app.use(controllerRouter)
 
 app.listen(3000, () => {
     console.log('** Listening on port 3000 **')
